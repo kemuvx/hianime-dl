717e437937d0f26df6732794048028f7e433b222
kemu <167982813+kemuvx@users.noreply.github.com>
2024-06-21 15:20:23 +0000

    fix bugs, fix subs, fix structure

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 # Work in progress
-## Currently, only anime with Japanese audio and eng subtitles are available for download. 
-## In some anime titles subs can be other language than english (will be fixed soonly)
-### Quality is probably only 360p.
-### Going to add hardsub to episodes.
-No dub because dubbed episodes are streamed in segments, making them difficult to download entirely at once. Japanese audio episodes, on the other hand, are downloaded as a single file at the beginning, allowing faster downloading.
 
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import shutil
 import requests
 from bs4 import BeautifulSoup
 from colorama import Fore
-from selenium.webdriver.common.by import By
 from seleniumbase import Driver
 import os
 
@@ -15,164 +14,198 @@ headers = {
     'Connection': 'keep-alive'}
 
 
-class Main:
-    def __init__(self):
-        print(Fore.LIGHTGREEN_EX + "HiAnime " + Fore.LIGHTWHITE_EX + "downloader")
+def replace_bad_num_in_m3u8_uri(url):
+    right_part = url.split('index-f')[1][1:]
+    return url.split('index-f')[0] + 'index-f' + right_part
 
+
+def get_rid_of_bad_chars(word):
+    bad_chars = ['-', '.', '/', '\\', '?', '%', '*', '<', '>', '|', '"', "[", "]", ":"]
+    for char in bad_chars:
+        word = word.replace(char, '')
+    return word
+
+
+def get_urls_to_animes_from_html(html_of_page):
+    # GETS URI FROM HIANIME SEARCH PAGE TO EVERY ANIME
+
+    urls_for_episodes = []
+    soup = BeautifulSoup(html_of_page, 'html.parser')
+    first_link = soup.find('div', id='episodes-content').find('a', class_='ssl-item ep-item active')['href']
+    print(first_link)
+    urls_for_episodes.append("https://hianime.to" + str(first_link))
+    links = soup.find('div', id='episodes-content').find_all('a', class_='ssl-item ep-item')
+    for link in links:
+        urls_for_episodes.append("https://hianime.to" + link['href'])
+
+    for url in urls_for_episodes:
+        print(url)
+    return urls_for_episodes
+
+
+class Main:
+
+    def __init__(self):
+        print(Fore.LIGHTGREEN_EX + "HiAnime " + Fore.LIGHTWHITE_EX + "Downloader")
+
+        # REMOVE TRASH
         try:
             os.remove('m3u8_To_MP4.mp4')
         except:
             pass
 
-        self.scrape_titles_from_search_page()
-        self.select_title()
-        self.scrape_m3u8_playlists_from_anime()
-        self.m3u8_to_mp4()
+        name_of_anime = input("Enter Name of Anime: ")
 
-    def scrape_titles_from_search_page(self):
-        def search_anime():
-            user_search = input(Fore.LIGHTWHITE_EX + "Enter an anime name: ")
-            self.anime_search_url = "https://hianime.to/search?keyword=" + user_search.replace(" ", "+")
-            anime_search_page = requests.get(self.anime_search_url, headers=headers)
-            return anime_search_page
-        anime_search_page = search_anime()
-        search_soup = BeautifulSoup(anime_search_page.text, 'html.parser')
-        main_content = search_soup.find('div', id='main-content')
+        # GET ANIME ELEMENTS FROM PAGE
+        url = "https://hianime.to/search?keyword=" + name_of_anime
+        search_page_response = requests.get(url)
+        search_page_soup = BeautifulSoup(search_page_response.content, 'html.parser')
+
+        main_content = search_page_soup.find('div', id='main-content')
         anime_elements = main_content.find_all('div', class_='flw-item')
 
         if not anime_elements:
             print("No anime found")
             Main()
 
-        counter_for_animes = 1
-        self.anime_elements_dict = {}
+        # MAKE DICT WITH ANIME TITLES
+        dict_with_anime_elements = {}
+        counter_for_anime_titles = 1
         for element in anime_elements:
-
-            anime_name = element.find('h3', class_='film-name').text
-            bad_chars = ['-', '.', '/', '\\', '?', '%', '*', '<', '>', '|', '"', "[", "]", ":"]
-            for char in bad_chars:
-                anime_name = anime_name.replace(char, '')
-
-            anime_link = "https://hianime.to" + element.find('a', class_='film-poster-ahref item-qtip')['href']
+            name_of_anime = get_rid_of_bad_chars(element.find('h3', class_='film-name').text)
+            url_of_anime = "https://hianime.to" + element.find('a', class_='film-poster-ahref item-qtip')['href']
             sub_episodes_available = element.find('div', class_="tick-item tick-sub").text
             try:
                 dub_episodes_available = element.find('div', class_="tick-item tick-dub").text
             except:
                 dub_episodes_available = "no"
 
-            self.anime_elements_dict[counter_for_animes] = {
-                'name': anime_name,
-                'link': anime_link,
+            dict_with_anime_elements[counter_for_anime_titles] = {
+                'name': name_of_anime,
+                'url': url_of_anime,
                 'sub_episodes': sub_episodes_available,
-                'dub_episodes': dub_episodes_available
-            }
-            counter_for_animes += 1
+                'dub_episodes': dub_episodes_available}
+            counter_for_anime_titles += 1
 
-    def select_title(self):
-        for i in range(len(self.anime_elements_dict)):
-            el = self.anime_elements_dict[i + 1]
+        # PRINT ANIME TITLES TO THE CONSOLE
+        for i in range(len(dict_with_anime_elements)):
+            el = dict_with_anime_elements[i + 1]
             print(
-                Fore.LIGHTRED_EX + str(i + 1) + ": " + Fore.LIGHTCYAN_EX + el['name'] + Fore.WHITE + ""
-                                                                                                     " | " + "Episodes: " +
+                Fore.LIGHTRED_EX + str(i + 1) + ": " + Fore.LIGHTCYAN_EX + el[
+                    'name'] + Fore.WHITE + " | " + "Episodes: " +
                 Fore.LIGHTYELLOW_EX + str(
                     el['sub_episodes']) + Fore.LIGHTWHITE_EX + " sub" + Fore.LIGHTGREEN_EX + " / "
                 + Fore.LIGHTYELLOW_EX + str(el['dub_episodes']) + Fore.LIGHTWHITE_EX + " dub")
 
-        self.number_of_anime = int(input("\nSelect an anime you want to download: "))
-        if self.number_of_anime == 0:
-            Main()
-        self.url_of_anime_watch = self.anime_elements_dict[self.number_of_anime]['link']
+        # USER SELECTS ANIME
+        number_of_anime = int(input("\nSelect an anime you want to download: "))
+        chosen_anime_dict = dict_with_anime_elements[number_of_anime]
 
-    def scrape_m3u8_playlists_from_anime(self):
-        self.m3u8_link = None
-        self.sub_link = None
+
+
+        # CHROME DRIVER
         try:
             print("Opening chrome driver...")
+            driver = Driver(mobile=True, wire=True, headless=True,
+                            extension_zip='extensions/CJPALHDLNBPAFIAMEJDNHCPHJBKEIAGM_1_58_0_0.zip')
 
-            driver = Driver(mobile=True, wire=True, headed=True, extension_zip='extensions/CJPALHDLNBPAFIAMEJDNHCPHJBKEIAGM_1_58_0_0.zip')
+            # CONNECT TO THE HIANIME
             print("Connecting to the website...\n")
+            driver.get(chosen_anime_dict['url'])
 
-            driver.get(self.url_of_anime_watch)
+            # GET URLS OF EPISODES
+            urls_for_episodes = get_urls_to_animes_from_html(driver.page_source)
+
+            # START SCRAPING URI'S TO .M3U8 AND .VTT
+
+            # LIST OF POSSIBLE  SUBTITLES LANGUAGES
+            lang_list = (
+            'ita', 'jpn', 'pol', 'por', 'ara', 'chi', 'cze', 'dan', 'dut', 'fin', 'fre', 'ger', 'gre', 'heb', 'hun', 'ind',
+            'kor', 'nob', 'pol', 'rum', 'rus', 'tha', 'vie', 'swe', 'spa', 'tur')
+
+            episode_counter = 1
+            used_vtt_uri_list = []
+            used_m3u8_uri_list = []
+            uri_links_list = []
+            counter_for_reload_page = 0
+
+            os.makedirs("m3u8_urls", exist_ok=True)
+            os.makedirs("vtt_files/"+chosen_anime_dict['name'], exist_ok=True)
+
+            # FILE WHERE LINKS TO M3U8 ARE WRITTEN m3u8_urls/some anime ep1.txt
+            m3u8_links_file = open("m3u8_urls/" + chosen_anime_dict['name'] + " m3u8"+".txt", 'w')
+            for url in urls_for_episodes:
+
+                print(
+                    Fore.LIGHTGREEN_EX + "Get" + Fore.LIGHTWHITE_EX + f" m3u8 link to episode {episode_counter}..." + Fore.LIGHTWHITE_EX)
+                driver.get(url)
+
+                uri_to_m3u8_is_scraped = False
+                uri_to_vtt_is_scraped = False
+
+                while not (uri_to_m3u8_is_scraped and uri_to_vtt_is_scraped):
+                    driver.sleep(3)
+
+                    # FIND M3U8 IN NETWORK REQUESTS
+                    for request in driver.requests:
+                        uri = request.url
+                        try:
+                            uri_without_f = replace_bad_num_in_m3u8_uri(uri)
+                            if uri.endswith(
+                                    '.m3u8') and 'index' in uri and uri_to_m3u8_is_scraped is False and uri_without_f not in used_m3u8_uri_list and uri_without_f is not None and "biananset" in uri:
+                                uri_without_f = replace_bad_num_in_m3u8_uri(uri)
+                                print('Found uri to m3u8: ' + uri)
+                                uri_to_m3u8_is_scraped = True
+                                m3u8_links_file.write("ep" + str(episode_counter) + ": " + uri + "\n")
+                                used_m3u8_uri_list.append(uri_without_f)
+                                uri_links_list.append(uri)
+                        except:
+                            pass
 
 
-            urls_for_episode_pages = []
-            webpage = driver.page_source
-            soup = BeautifulSoup(webpage, 'html.parser')
-            first_link = soup.find('div', id='episodes-content').find('a', class_='ssl-item ep-item active')
-            urls_for_episode_pages.append("https://hianime.to"+ first_link.get('href'))
-            links = soup.find('div', id='episodes-content').find_all('a', class_='ssl-item ep-item')
-            for link in links:
-                urls_for_episode_pages.append("https://hianime.to"+ link.get('href'))
-
-            for url in urls_for_episode_pages:
-                print(url)
 
 
 
-            self.uri_links = []
-            used_urls = []
-            used_sub_urls = []
-            counter_for_subs = 1
-            counter_for_episodes = 1
-            sub_url_already_scraped = False
-            os.makedirs('m3u8_urls', exist_ok=True)
-
-            with open("m3u8_urls/" + self.anime_elements_dict[self.number_of_anime]['name'] + ".txt", 'w') as file:
-                for url in urls_for_episode_pages:
-                    print(url)
+                    # FIND VTT IN NETWORK REQUESTS
+                    for request in driver.requests:
+                        uri = request.url
+                        if uri.endswith(".vtt") and "thumbnails" not in uri and not uri_to_vtt_is_scraped and not any(ele in uri for ele in lang_list) and uri not in used_vtt_uri_list:
+                            print("Found uri to vtt: "+uri)
+                            used_vtt_uri_list.append(uri)
+                            uri_to_vtt_is_scraped = True
+                            with open("vtt_files/" + chosen_anime_dict['name'] + "/" +
+                                      chosen_anime_dict['name'] + " subs " + "ep" + str(
+                                    episode_counter) + ".vtt", 'wb') as subs_file:
+                                content_of_uri = requests.get(uri, headers=headers).content
+                                subs_file.write(content_of_uri)
+                episode_counter += 1
+                counter_for_reload_page += 1
+                if counter_for_reload_page == 5:
+                    counter_for_reload_page = 0
                     driver.get(url)
-                    print(Fore.LIGHTGREEN_EX + "Get" + Fore.LIGHTWHITE_EX + f" m3u8 link to episode {counter_for_episodes}..." + Fore.LIGHTWHITE_EX)
-
-                    counter_for_f5 = 0
-                    while self.m3u8_link is None and self.sub_link is None:
-                        driver.sleep(1.5)
-
-                        for request in driver.requests:
-                            if request.url.endswith(
-                                    '.m3u8') and "index" in request.url and request.url not in used_urls:
-                                used_urls.append(request.url)
-                                self.m3u8_link = request.url
-                                file.write("ep" + str(counter_for_episodes) + ": " + self.m3u8_link + "\n")
-                                sub_url_already_scraped = False
-                            elif request.url.endswith('.vtt') and "thumbnails" not in request.url and sub_url_already_scraped is False and request.url not in used_sub_urls:
-                                os.makedirs(f'subs_for_anime/' + self.anime_elements_dict[self.number_of_anime]['name'], exist_ok=True)
-                                with open(f"subs_for_anime/" + self.anime_elements_dict[self.number_of_anime]['name'] +"/" + self.anime_elements_dict[self.number_of_anime]['name'] + " subs " + "ep" +str(counter_for_subs) + ".vtt", 'wb') as subs_file:
-                                    r = requests.get(request.url, headers=headers)
-                                    used_sub_urls.append(request.url)
-                                    subs_file.write(r.content)
-                                    sub_url_already_scraped = True
-                        counter_for_f5 += 1
-                        if counter_for_f5 > 5:
-                            driver.get(url)
-                            counter_for_f5 = 0
-
-                    self.uri_links.append(self.m3u8_link)
-                    self.m3u8_link = None
-                    self.sub_link = None
-
-                    counter_for_f5 = 0
-                    counter_for_subs += 1
-                    counter_for_episodes += 1
-                    driver.sleep(1)
-
-
         except Exception as e:
             raise e
         finally:
-            file.close()
-            driver.quit()
-            print(Fore.LIGHTWHITE_EX + "Driver closed")
+            try:
+                m3u8_links_file.close()
+            except:
+                pass
 
-    def m3u8_to_mp4(self):
+            driver.quit()
+            print("Driver closed")
+
+        # DOWNLOAD MP4 FROM M3U8
+        download_or_no = input("Do you want to download m3u8 files now?\nType 0 to exit")
+        if download_or_no == "0": quit(0)
         import m3u8_To_MP4
-        folder_name = self.anime_elements_dict[self.number_of_anime]['name'] + "/"
+        folder_name = chosen_anime_dict['name'] + "/"
         os.makedirs('./mp4_out/' + folder_name, exist_ok=True)
         i = 1
-        for uri in self.uri_links:
+        for uri in uri_links_list:
             print(Fore.LIGHTYELLOW_EX + "\nDownloading episode " + str(i) + Fore.WHITE)
             m3u8_To_MP4.multithread_download(uri)
-            name = self.anime_elements_dict[self.number_of_anime]['name'] + " ep" + str(i) + ".mp4"
-            folder_name = self.anime_elements_dict[self.number_of_anime]['name'] + "/"
+            name = chosen_anime_dict['name'] + " ep" + str(i) + ".mp4"
+            folder_name = chosen_anime_dict['name'] + "/"
             os.rename("m3u8_To_MP4.mp4", name)
             shutil.move(name, "./mp4_out/" + folder_name + name)
             print(Fore.LIGHTYELLOW_EX + "\nEpisode " + str(i) + " has been downloaded" + Fore.WHITE)
